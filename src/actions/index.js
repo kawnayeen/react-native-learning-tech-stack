cd1e930a020cef8811facbc7b54dcb2aa40864ff
kawnayeen <kawnayeen.08@gmail.com>
2017-04-17 12:44:24 +0600

    on tapping topic, description is showed

---
+++ src/actions/index.js
@@ -0,0 +1,7 @@
+/**
+ * Created by kawnayeen on 4/17/17.
+ */
+export const selectLibrary = (libraryId) => ({
+    type: 'select_library',
+    payload: libraryId
+});
--- src/reducers/selection.reducer.js
+++ src/reducers/selection.reducer.js
@@ -1,4 +1,11 @@
 /**
  * Created by kawnayeen on 4/17/17.
  */
-export default () => null;
+export default (state = null, action) => {
+    switch (action.type) {
+        case 'select_library':
+            return action.payload;
+        default:
+            return state;
+    }
+};
